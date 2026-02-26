c13897029f34b97fd2665c1293277853bd762b9f
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-02-26 23:49:23 +0530

    Create sync_counter_design.v

---
+++ Day 8/Counters/Synchronous counter/sync_counter_design.v
@@ -0,0 +1,16 @@
+module synchronous_counter(
+    input  clk,
+    input  rst,
+    output reg [2:0] count = 3'b000
+);
+
+  always @(posedge clk) begin
+    if (rst)
+      count <= 3'b000;
+    else if (count == 3'b111)
+      count <= 3'b000;
+    else
+      count <= count + 1;
+  end
+
+endmodule
