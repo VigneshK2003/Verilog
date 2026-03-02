b15832674a99ee0676f4ed0fbbf0c3324c960224
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-02 22:49:10 +0530

    Refactor testbench for synchronous down counter

---
+++ Day 8/Counters/Synchronous Down Counter/synchronous_down_counter_tb.v
@@ -0,0 +1,19 @@
+module sync_counter_tb;
+            reg clk, rst;
+            wire [2:0]count;
+  
+sync_down_counter dut (.clk(clk), .rst(rst), .count(count) );
+
+       always #5 clk = ~clk;
+
+ initial begin
+    $monitor("time = %0t, clk = %b, rst = %b, count = %b",$time,clk,rst,count);
+    $dumpfile("sync_down_counter.vcd");
+    $dumpvars(0, sync_counter_tb);
+
+   clk = 0; rst = 1;
+   #5 rst = 0;
+
+   #100 $finish;
+  end
+endmodule
