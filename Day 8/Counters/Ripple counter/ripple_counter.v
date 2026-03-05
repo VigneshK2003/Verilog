88100f0e96d39bef8709725c432a0deb0c59e754
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-05 23:59:29 +0530

    Create ripple_counter.v

---
+++ Day 8/Counters/Ripple counter/ripple_counter.v
@@ -0,0 +1,14 @@
+module ripple_counter(
+               input rst, clk,
+               output reg [2:0]count
+);
+
+   always@(posedge clk or negedge rst) begin
+       if(!rst)
+           count <= 3'b000;
+       else if(count == 3'b111)
+           count <= 3'b000;
+       else
+           count <= count + 1;
+      end
+endmodule
