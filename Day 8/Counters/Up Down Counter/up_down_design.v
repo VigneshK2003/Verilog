313d4733855085bfd1b99070333392d9cbff76f3
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-07 23:57:40 +0530

    Implement up/down counter module in Verilog

---
+++ Day 8/Counters/Up Down Counter/up_down_design.v
@@ -0,0 +1,17 @@
+module up_down #(parameter WIDTH = 4)(
+          input clk,
+          input rst,
+          input up_ena,
+          output reg [WIDTH-1:0] count
+);
+
+  always@(posedge clk or posedge rst)
+    begin
+       if(rst)
+          count <= 1'b0;
+       else if(up_ena)
+          count <= count + 1'b1;
+      else
+          count <= count - 1'b1;
+     end
+endmodule
