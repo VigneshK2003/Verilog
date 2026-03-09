16b483e1ff82fb5a85bde7bedb4594d94f7e9122
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-09 23:29:34 +0530

    Create Ring_Counter.v

---
+++ Day 8/Counters/Ring Counter/Ring_Counter.v
@@ -0,0 +1,13 @@
+module ring_counter #(parameter N = 4)(
+               input rst, clk,
+               output reg [N-1:0]count
+);
+
+        always@(posedge clk) begin
+           if(rst)
+                count <= 4'b0001;
+           else
+                count <= { count[N-2:0], count[N-1] };
+           end
+
+endmodule
