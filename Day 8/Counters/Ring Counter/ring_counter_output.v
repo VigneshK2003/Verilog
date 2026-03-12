5dc87d747ae718da79d7446f802a2e6afa93a2e5
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-12 23:41:47 +0530

    Create ring_counter_output.v

---
+++ Day 8/Counters/Ring Counter/ring_counter_output.v
@@ -0,0 +1,14 @@
+VCD info: dumpfile ring_counter.vcd opened for output.
+Time = 0, clk = 0, rst = 1, count = 0000
+Time = 5, clk = 1, rst = 1, count = 0001
+Time = 10, clk = 0, rst = 0, count = 0001
+Time = 15, clk = 1, rst = 0, count = 0010
+Time = 20, clk = 0, rst = 0, count = 0010
+Time = 25, clk = 1, rst = 0, count = 0100
+Time = 30, clk = 0, rst = 0, count = 0100
+Time = 35, clk = 1, rst = 0, count = 1000
+Time = 40, clk = 0, rst = 0, count = 1000
+Time = 45, clk = 1, rst = 0, count = 0001
+Time = 50, clk = 0, rst = 0, count = 0001
+Time = 55, clk = 1, rst = 0, count = 0010
+ring_counter_tb.v:21: $finish called at 60 (1s)
