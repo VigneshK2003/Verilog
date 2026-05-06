df5ab53aa5ccf130324ff297415a8ccc1d1b9044
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-05-06 18:52:25 +0530

    Add comments for full adder output clarity
    Added comments to clarify the full adder output.

---
+++ DAY 2/Full adder using concatenation operator/output.v
@@ -0,0 +1,11 @@
+// full adder output
+
+Time = 0, A = 0, B = 0, Cin = 0, sum = 0, carry = 0
+ Time = 10, A = 0, B = 0, Cin = 1, sum = 1, carry = 0
+ Time = 20, A = 0, B = 1, Cin = 0, sum = 1, carry = 0
+ Time = 30, A = 0, B = 1, Cin = 1, sum = 0, carry = 1
+ Time = 40, A = 1, B = 0, Cin = 0, sum = 1, carry = 0
+ Time = 50, A = 1, B = 0, Cin = 1, sum = 0, carry = 1
+ Time = 60, A = 1, B = 1, Cin = 0, sum = 0, carry = 1
+ Time = 70, A = 1, B = 1, Cin = 1, sum = 1, carry = 1
+fa_tb.v:23: $finish called at 90 (1s)
