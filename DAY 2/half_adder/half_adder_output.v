cd082659f41f75578720cdce2c1ae64291c4cf74
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-05-13 18:41:20 +0530

    Create half_adder_output.v

---
+++ DAY 2/half_adder/half_adder_output.v
@@ -0,0 +1,7 @@
+
+VCD info: dumpfile half_adder.vcd opened for output.
+$Time=0 , a=0, b=0 ,s =0, c=0
+$Time=10 , a=0, b=1 ,s =1, c=0
+$Time=20 , a=1, b=0 ,s =1, c=0
+$Time=30 , a=1, b=1 ,s =0, c=1
+half_adder_tb.v:10: $finish called at 40 (1s)
