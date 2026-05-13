728d17e73ea2d084e68f3a78771b184d936f9caf
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-05-13 18:37:41 +0530

    Add simulation output for full subtractor
    Output results for full subtractor simulation with various input combinations.

---
+++ DAY 2/full_subtractor/full_subtractor_output.v
@@ -0,0 +1,11 @@
+
+VCD info: dumpfile full_subtractor.vcd opened for output.
+Time=0 , A=0, B=0, Bin=0, Diff=0, Borrow=0
+Time=10 , A=0, B=0, Bin=1, Diff=1, Borrow=1
+Time=20 , A=0, B=1, Bin=0, Diff=1, Borrow=1
+Time=30 , A=0, B=1, Bin=1, Diff=0, Borrow=1
+Time=40 , A=1, B=0, Bin=0, Diff=1, Borrow=0
+Time=50 , A=1, B=0, Bin=1, Diff=0, Borrow=0
+Time=60 , A=1, B=1, Bin=0, Diff=0, Borrow=0
+Time=70 , A=1, B=1, Bin=1, Diff=1, Borrow=1
+full_subtractor_tb.v:14: $finish called at 80 (1s)
