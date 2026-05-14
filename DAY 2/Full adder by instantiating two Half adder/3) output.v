521c3e748661db51fe5523b086963c630e3eef10
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-05-14 11:22:38 +0530

    Add simulation output for full adder
    Added output data for full adder simulation with various input combinations.

---
+++ DAY 2/Full adder by instantiating two Half adder/3) output.v
@@ -0,0 +1,9 @@
+VCD info: dumpfile  fa.vcd opened for output.
+ A = 0, B = 1, Cin = 1, sum = 0, carry = 1
+ A = 1, B = 1, Cin = 1, sum = 1, carry = 1
+ A = 1, B = 0, Cin = 1, sum = 0, carry = 1
+ A = 0, B = 1, Cin = 0, sum = 1, carry = 0
+ A = 1, B = 1, Cin = 0, sum = 0, carry = 1
+ A = 1, B = 0, Cin = 0, sum = 1, carry = 0
+ A = 0, B = 1, Cin = 0, sum = 1, carry = 0
+ A = 1, B = 1, Cin = 1, sum = 1, carry = 1
