d8fb7d416ec60d611173861aad55e717d23ca881
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-05-17 13:58:57 +0530

    Add simulation output for Mealy FSM
    Added simulation output for Mealy FSM testbench.

---
+++ Day 9/1011 - Mealy FSM/03) output.v
@@ -0,0 +1,13 @@
+ Time = 0, clk = 0, rst = 1, data_in = 0, data_out = 0
+ Time = 5, clk = 1, rst = 1, data_in = 0, data_out = 0
+ Time = 10, clk = 0, rst = 0, data_in = 1, data_out = 0
+ Time = 15, clk = 1, rst = 0, data_in = 1, data_out = 0
+ Time = 20, clk = 0, rst = 0, data_in = 0, data_out = 0
+ Time = 25, clk = 1, rst = 0, data_in = 0, data_out = 0
+ Time = 30, clk = 0, rst = 0, data_in = 1, data_out = 0
+ Time = 35, clk = 1, rst = 0, data_in = 1, data_out = 0
+ Time = 40, clk = 0, rst = 0, data_in = 1, data_out = 0
+ Time = 45, clk = 1, rst = 0, data_in = 1, data_out = 1
+ Time = 50, clk = 0, rst = 0, data_in = 0, data_out = 1
+ Time = 55, clk = 1, rst = 0, data_in = 0, data_out = 0
+$finish called from file "testbench.sv", line 30.
