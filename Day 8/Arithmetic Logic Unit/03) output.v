0ca11b5bbe7a926f21e7cd99e533dfe7cfd98b56
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-05-25 10:19:20 +0530

    Add output log for arithmetic logic unit
    Add output log for arithmetic operations with time, A, B, opcode, and result.

---
+++ Day 8/Arithmetic Logic Unit/03) output.v
@@ -0,0 +1,5 @@
+
+Time = 0, A = 00000101, B = 00001000, opcode = 0000, result = 00001101
+Time = 10, A = 00000101, B = 00001000, opcode = 0010, result = 00000000
+Time = 20, A = 00000101, B = 00001000, opcode = 0011, result = 00001101
+$finish called from file "testbench.sv", line 28.
