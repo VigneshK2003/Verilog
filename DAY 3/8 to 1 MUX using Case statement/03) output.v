8905d2d11770d6970c7e1310f5296f7495087bb4
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-06-08 12:23:51 +0530

    Implement output logging for mux simulation
    Added output log for 8 to 1 multiplexer simulation.

---
+++ DAY 3/8 to 1 MUX using Case statement/03) output.v
@@ -0,0 +1,10 @@
+VCD info: dumpfile mux8to1.vcd opened for output.
+ Time = 0, sel = 000, out = 10
+ Time = 10, sel = 101, out = 60
+ Time = 20, sel = 010, out = 30
+ Time = 30, sel = 111, out = 80
+ Time = 40, sel = 001, out = 20
+ Time = 50, sel = 101, out = 60
+ Time = 60, sel = 011, out = 40
+ Time = 70, sel = 100, out = 50
+mux_tb.v:27: $finish called at 80 (1s)
