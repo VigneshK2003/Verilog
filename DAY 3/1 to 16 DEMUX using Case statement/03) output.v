1eb3dc8285e14bbbf444905ffbbf16981dee1c11
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-06-10 09:21:07 +0530

    Update 03) output.v

--- DAY 3/1 to 16 DEMUX using Case statement/03) output.v
+++ DAY 3/1 to 16 DEMUX using Case statement/03) output.v
@@ -1,1 +1,6 @@
 
+ Time = 0, in =  40, sel = 0101, out3 = 0, out5 = 40, out9 = 0, out12 = 0
+ Time = 10, in =  50, sel = 1100, out3 = 0, out5 = 0, out9 = 0, out12 = 50
+ Time = 20, in =  70, sel = 1001, out3 = 0, out5 = 0, out9 = 70, out12 = 0
+ Time = 30, in =  80, sel = 0011, out3 = 80, out5 = 0, out9 = 0, out12 = 0
+ demux_tb.v:26: $finish called at 40 (1s)
