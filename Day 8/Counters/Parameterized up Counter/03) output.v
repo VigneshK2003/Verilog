8fc32771155c078d7cb276dff257c7edf7367e50
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-06-11 10:49:13 +0530

    Add simulation output for parameterized up counter

---
+++ Day 8/Counters/Parameterized up Counter/03) output.v
@@ -0,0 +1,24 @@
+time = 0, rst = 1, clk = 0, count = xxxx
+time = 5, rst = 1, clk = 1, count = 0000
+time = 10, rst = 0, clk = 0, count = 0000
+time = 15, rst = 0, clk = 1, count = 0001
+time = 20, rst = 0, clk = 0, count = 0001
+time = 25, rst = 0, clk = 1, count = 0010
+time = 30, rst = 0, clk = 0, count = 0010
+time = 35, rst = 0, clk = 1, count = 0011
+time = 40, rst = 0, clk = 0, count = 0011
+time = 45, rst = 0, clk = 1, count = 0100
+time = 50, rst = 0, clk = 0, count = 0100
+time = 55, rst = 0, clk = 1, count = 0101
+time = 60, rst = 0, clk = 0, count = 0101
+time = 65, rst = 0, clk = 1, count = 0110
+time = 70, rst = 0, clk = 0, count = 0110
+time = 75, rst = 0, clk = 1, count = 0111
+time = 80, rst = 0, clk = 0, count = 0111
+time = 85, rst = 0, clk = 1, count = 1000
+time = 90, rst = 0, clk = 0, count = 1000
+time = 95, rst = 0, clk = 1, count = 1001
+time = 100, rst = 0, clk = 0, count = 1001
+time = 105, rst = 0, clk = 1, count = 1010
+$finish called from file "testbench.sv", line 19.
+$finish at simulation time                  110
