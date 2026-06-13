735fe14a643abd3cbb83a418ac15f67789428dc5
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-06-13 18:07:27 +0530

    Create 03) output.v

---
+++ DAY 2/Parameterized Full Adder module/03) output.v
@@ -0,0 +1,11 @@
+
+Time=0, a=0000, b=0000, cin=0, sum=0000, carry=0
+Time=10, a=0100, b=0010, cin=1, sum=0111, carry=0
+Time=20, a=0000, b=0001, cin=0, sum=0001, carry=0
+Time=30, a=0000, b=0001, cin=1, sum=0010, carry=0
+Time=40, a=0001, b=0000, cin=0, sum=0001, carry=0
+Time=50, a=0001, b=0000, cin=1, sum=0010, carry=0
+Time=60, a=0001, b=0001, cin=0, sum=0010, carry=0
+Time=70, a=0001, b=0001, cin=1, sum=0011, carry=0
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 80 ns
