bb2546492d74aa65045c42d20eb0c8ca339cd6ce
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-06-16 13:20:53 +0530

    Create 01) design.v

---
+++ Day 7/Shift Register from FF using Generate Block/01) design.v
@@ -0,0 +1,32 @@
+
+module d_ff(
+         input clk, d, rst,
+        output q
+);
+  
+  always@(posedge clk)begin
+    if(rst)
+       q <= '0;
+    else
+       q <= d;
+  end
+endmodule
+    
+module shift_reg#(parameter width = 4)(
+        input clk,rst,
+        input [width-1:0]d,
+        output [width-1:0]q
+);
+  
+  genvar i;
+  wire [width:0]c;
+  assign c[0] = d[0];
+  
+  generate
+    for(i=0; i<width; i++)begin
+      d_ff dut( .d(c[i]), .q(c[i+1]), .clk(clk), .rst(rst) );
+    end
+  endgenerate
+  assign q = c[width:1];
+endmodule
+    
