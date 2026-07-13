d66eeb13571cea2af8113184b0b76c0b9532aa55
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-07-13 22:42:38 +0530

    Add output waveform simulation data for shift register

---
+++ Day 7/Shift Register from FF using Generate Block/03) output.v
@@ -0,0 +1,16 @@
+
+ Time = 0, clk = 0, rst = 1, d = 0000, q = 0000
+ Time = 5, clk = 1, rst = 1, d = 0000, q = 0000
+ Time = 10, clk = 0, rst = 0, d = 0101, q = 0101
+ Time = 15, clk = 1, rst = 0, d = 0101, q = 0101
+ Time = 20, clk = 0, rst = 0, d = 0101, q = 0101
+ Time = 25, clk = 1, rst = 0, d = 0101, q = 0101
+ Time = 30, clk = 0, rst = 0, d = 1000, q = 1000
+ Time = 35, clk = 1, rst = 0, d = 1000, q = 1000
+ Time = 40, clk = 0, rst = 0, d = 1000, q = 1000
+ Time = 45, clk = 1, rst = 0, d = 1000, q = 1000
+ Time = 50, clk = 0, rst = 0, d = 1010, q = 1010
+ Time = 55, clk = 1, rst = 0, d = 1010, q = 1010
+ Time = 60, clk = 0, rst = 0, d = 1010, q = 1010
+ Time = 65, clk = 1, rst = 0, d = 1010, q = 1010
+$finish called from file "testbench.sv", line 107.
