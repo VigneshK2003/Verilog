c2b3395f73a2086cf6aeb06a1c829e2f3c9ba00d
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-07-20 22:54:02 +0530

    Implement output for 8-to-1 MUX using test cases
    Add output values for 8-to-1 multiplexer simulation.

---
+++ DAY 4/8 to 1 MUX using Ternary Operator/03) output.v
@@ -0,0 +1,7 @@
+
+ in0 = 0, in1 = 1, in2 = 1, in3 = 0, in4 = 0, in5 = 1, in6 = 0, in7 = 1, sel = 000, Y = 0
+ in0 = 0, in1 = 1, in2 = 1, in3 = 0, in4 = 0, in5 = 1, in6 = 0, in7 = 1, sel = 001, Y = 1
+ in0 = 0, in1 = 1, in2 = 1, in3 = 0, in4 = 0, in5 = 1, in6 = 0, in7 = 1, sel = 110, Y = 1
+ in0 = 0, in1 = 1, in2 = 1, in3 = 0, in4 = 0, in5 = 1, in6 = 0, in7 = 1, sel = 010, Y = 1
+$finish called from file "testbench.sv", line 25.
+$finish at simulation time                   40
